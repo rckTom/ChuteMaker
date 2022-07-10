4f905f137a5f3150a0589be721b7b552fe0d8745
Thomas Schmid <tom@lfence.de>
2022-07-10 23:11:15 +0200

    util.py: add arrow drawing utility
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- util.py
+++ util.py
@@ -18,3 +18,19 @@ import math
 
 def mm_to_pt(mm):
     return mm * 72/25.4
+
+def arrow(ctx, start, end, head_size=10, arrow_start = False, arrow_end = True):
+    angle = math.atan2(end[1] - start[1], end[0] - start[0])
+
+    ctx.move_to(start)
+    if arrow_start:
+        ctx.rel_line_to(head_size * math.cos(-angle - math.pi + 30*math.pi/180), head_size * math.sin(-angle - math.pi + 30*math.pi/180))
+        ctx.move_to(start)
+        ctx.rel_line_to(head_size * math.cos(-angle - math.pi - 30*math.pi/180), head_size * math.sin(-angle - math.pi - 30*math.pi/180))
+        ctx.move_to(start)  
+    
+    ctx.line_to(end)
+    if arrow_end:
+        ctx.rel_line_to(head_size * math.cos(angle - math.pi + 30*math.pi/180), head_size * math.sin(angle - math.pi + 30*math.pi/180))
+        ctx.move_to(end)
+        ctx.rel_line_to(head_size * math.cos(angle - math.pi - 30*math.pi/180), head_size * math.sin(angle - math.pi - 30*math.pi/180))
